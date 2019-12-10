64f635e1d73aeff8ec499fbd914c110db5654d54
Chris LaRose <cjlarose@gmail.com>
2019-12-10 14:55:18 -0800

    Extract withSuccessfulParse

--- src/Advent2019/Day1.hs
+++ src/Advent2019/Day1.hs
@@ -3,27 +3,18 @@ module Advent2019.Day1
   , fuelRequirementsForMass
   ) where
 
-import System.IO (hPutStrLn, stderr)
-import System.Exit (exitFailure)
-
 import Text.Parsec (ParseError, many1, sepEndBy1)
 import qualified Text.Parsec (parse)
 import Text.Parsec.Char (endOfLine, digit)
 import Text.Parsec.ByteString (Parser)
 
-import Advent2019.Input (getProblemInputAsByteString)
+import Advent2019.Input (getProblemInputAsByteString, withSuccessfulParse)
 
 moduleMass :: Parser Int
 moduleMass = read <$> many1 digit
 
 modules :: Parser [Int]
 modules = sepEndBy1 moduleMass endOfLine
-
-handleParseError :: ParseError -> IO ()
-handleParseError err = do
-  hPutStrLn stderr "Parse error:"
-  hPutStrLn stderr . show $ err
-  exitFailure
 
 requiredFuel :: Int -> Int
 requiredFuel mass = (mass `div` 3) - 2
@@ -45,5 +36,4 @@ printResults xs = do
 solve :: IO ()
 solve = do
   input <- getProblemInputAsByteString 1
-  let parseResult = Text.Parsec.parse modules "" input
-  either handleParseError printResults parseResult
+  withSuccessfulParse modules printResults input
--- src/Advent2019/Input.hs
+++ src/Advent2019/Input.hs
@@ -1,9 +1,16 @@
 module Advent2019.Input
   ( getProblemInputAsString
   , getProblemInputAsByteString
+  , withSuccessfulParse
   ) where
 
+import System.IO (hPutStrLn, stderr)
+import System.Exit (exitFailure)
+
 import qualified Data.ByteString as B
+import Text.Parsec (ParseError)
+import qualified Text.Parsec (parse)
+import Text.Parsec.ByteString (Parser)
 
 path :: Int -> String
 path problemNumber = "inputs/" ++ (show problemNumber) ++ ".txt"
@@ -13,3 +20,12 @@ getProblemInputAsString = readFile . path
 
 getProblemInputAsByteString :: Int -> IO B.ByteString
 getProblemInputAsByteString = B.readFile . path
+
+handleParseError :: ParseError -> IO ()
+handleParseError err = do
+  hPutStrLn stderr "Parse error:"
+  hPutStrLn stderr . show $ err
+  exitFailure
+
+withSuccessfulParse :: Parser a -> (a -> IO ()) -> B.ByteString -> IO ()
+withSuccessfulParse p f input = either handleParseError f . Text.Parsec.parse p "" $ input
