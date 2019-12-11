1328f43e8515dfcc403aa024a82c9d07cbaf86e7
Chris LaRose <cjlarose@gmail.com>
2019-12-11 01:21:39 -0800

    Complete Day 2, Part 1

--- src/Advent2019/Day2.hs
+++ src/Advent2019/Day2.hs
@@ -1,6 +1,11 @@
 module Advent2019.Day2
   ( solve
+  , runProgram
   ) where
+
+import Data.Array.IArray (IArray, listArray, (!), (//))
+import Data.Array.Unboxed (UArray)
+import Control.Monad.State (State, get, put, evalState)
 
 import Text.Parsec (many1, sepBy1, eof)
 import Text.Parsec.Char (endOfLine, digit, char)
@@ -13,9 +18,60 @@ program = sepBy1 instruction (char ',') <* endOfLine <* eof
   where
     instruction = read <$> many1 digit
 
+data MachineState = Running | Terminated
+type Machine = (Int, UArray Int Int, MachineState)
+
+newMachine :: [Int] -> Machine
+newMachine xs = (0, arr, Running)
+  where
+    n = length xs
+    arr = listArray (0, n - 1) xs
+
+dereference :: UArray Int Int -> Int -> Int
+dereference arr i = arr ! address
+  where address = arr ! i
+
+executeBinaryOp :: (Int -> Int -> Int) -> UArray Int Int -> Int -> UArray Int Int
+executeBinaryOp f memory pc = memory // [(dest, res)]
+  where
+    operand1 = dereference memory $ pc + 1
+    operand2 = dereference memory $ pc + 2
+    dest = memory ! (pc + 3)
+    res = operand1 `f` operand2
+
+executeOneInstruction :: State Machine ()
+executeOneInstruction = do
+  (pc, memory, _) <- get
+  let opcode = memory ! pc
+  case opcode of
+    1 -> do
+      let newMemory = executeBinaryOp (+) memory pc
+      put (pc + 4, newMemory, Running)
+    2 -> do
+      let newMemory = executeBinaryOp (*) memory pc
+      put (pc + 4, newMemory, Running)
+    99 -> put (pc, memory, Terminated)
+
+runMachine :: State Machine Int
+runMachine = do
+  (_, memory, status) <- get
+  case status of
+    Terminated -> return $ memory ! 0
+    Running -> executeOneInstruction >> runMachine
+
+updateMemory :: [(Int, Int)] -> State Machine ()
+updateMemory updates = do
+  (pc, memory, status) <- get
+  let newMemory = memory // updates
+  put (pc, newMemory, status)
+
+runProgram :: [Int] -> [(Int, Int)] -> Int
+runProgram xs updates = evalState (updateMemory updates >> runMachine) (newMachine xs)
+
 printResults :: [Int] -> IO ()
 printResults xs = do
-  putStrLn . show $ xs
+  let part1 = runProgram xs [(1, 12), (2, 2)]
+  putStrLn . show $ part1
 
 solve :: IO ()
 solve = getProblemInputAsByteString 2 >>= withSuccessfulParse program printResults
