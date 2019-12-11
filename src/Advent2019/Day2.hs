d6b7d15cd3eb9d1d2a3ebc4c2f6a641ad2f5dcee
Chris LaRose <cjlarose@gmail.com>
2019-12-11 01:34:02 -0800

    Complete Day 2, Part 2

--- src/Advent2019/Day2.hs
+++ src/Advent2019/Day2.hs
@@ -3,6 +3,8 @@ module Advent2019.Day2
   , runProgram
   ) where
 
+import Data.List (find)
+import Data.Maybe (fromJust)
 import Data.Array.IArray (IArray, listArray, (!), (//))
 import Data.Array.Unboxed (UArray)
 import Control.Monad.State (State, get, put, evalState)
@@ -71,10 +73,15 @@ runProgram xs updates = evalState (updateMemory updates >> runMachine) (newMachi
 runProgramWithInputs :: [Int] -> (Int, Int) -> Int
 runProgramWithInputs xs (a, b) = runProgram xs [(1, a), (2, b)]
 
+findNounVerb :: [Int] -> Int -> (Int, Int)
+findNounVerb xs n = fst . fromJust . find ((== n) . snd) . map (\p -> (p, runProgramWithInputs xs p)) $ [(a, b) | a <- [0..99], b <- [0..99]]
+
 printResults :: [Int] -> IO ()
 printResults xs = do
   let part1 = runProgramWithInputs xs (12, 2)
+  let (noun, verb) = findNounVerb xs 19690720
   putStrLn . show $ part1
+  putStrLn . show $ 100 * noun + verb
 
 solve :: IO ()
 solve = getProblemInputAsByteString 2 >>= withSuccessfulParse program printResults
