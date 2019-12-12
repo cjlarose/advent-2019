2d2dd8afe6e8ca7757fc2fbf17b48f2f9ccfbd37
Chris LaRose <cjlarose@gmail.com>
2019-12-11 20:31:02 -0800

    Complete day 4, part 1

--- src/Advent2019/Day4.hs
+++ src/Advent2019/Day4.hs
@@ -2,6 +2,9 @@ module Advent2019.Day4
   ( solve
   ) where
 
+import Data.List (find)
+import Data.Maybe (isJust)
+
 import Text.Parsec (many1, eof)
 import Text.Parsec.Char (digit, char)
 import Text.Parsec.ByteString (Parser)
@@ -12,9 +15,27 @@ range :: Parser (Int, Int)
 range = (\min max -> (min, max)) <$> (integer <* char '-') <*> (integer <* eof)
   where integer = read <$> many1 digit
 
+pairs :: [a] -> [(a, a)]
+pairs xs = zip xs (tail xs)
+
+hasRepeatedDigitPair :: Int -> Bool
+hasRepeatedDigitPair x = isJust . find (\(a, b) -> a == b) . pairs . show $ x
+
+monotonicallyIncreasing :: Ord a => [a] -> Bool
+monotonicallyIncreasing [] = True
+monotonicallyIncreasing [x] = True
+monotonicallyIncreasing (a:b:rest) = a <= b && monotonicallyIncreasing (b:rest)
+
+hasMonotonicallyIncreasingDigits :: Int -> Bool
+hasMonotonicallyIncreasingDigits = monotonicallyIncreasing . show
+
+isCandidate :: Int -> Bool
+isCandidate x = hasMonotonicallyIncreasingDigits x && hasRepeatedDigitPair x
+
 printResults :: (Int, Int) -> IO ()
 printResults (min, max) = do
-  print (min, max)
+  let numCandidates = length . filter isCandidate $ [min..max]
+  print numCandidates
 
 solve :: IO ()
 solve = getProblemInputAsByteString 4 >>= withSuccessfulParse range printResults
