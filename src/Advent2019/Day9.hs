37fbcffd1aa6e37f1d45a5351af966482bbeca04
Chris LaRose <cjlarose@gmail.com>
2019-12-17 01:54:53 -0800

    Pass in program mode

--- src/Advent2019/Day9.hs
+++ src/Advent2019/Day9.hs
@@ -6,18 +6,18 @@ import Advent2019.Input (getProblemInputAsByteString, withSuccessfulParse)
 import Advent2019.Intcode.Parse (program)
 import Advent2019.Intcode.Execute (runMachineWithInput)
 
-runProgramInTestMode :: [Integer] -> [Integer]
-runProgramInTestMode program = runMachineWithInput program $ [1]
+testMode = 1
+sensorBoostMode = 2
 
-runProgramInSensorBoostMode :: [Integer] -> [Integer]
-runProgramInSensorBoostMode program = runMachineWithInput program $ [2]
+runProgramInMode :: Integer -> [Integer] -> [Integer]
+runProgramInMode mode program = runMachineWithInput program [mode]
 
 printResults :: [Integer] -> (String, String)
 printResults program = (part1, part2)
   where
-    boostKeycode = last . runProgramInTestMode $ program
+    boostKeycode = last . runProgramInMode 1 $ program
     part1 = show boostKeycode
-    distressSignalCoordinates = last . runProgramInSensorBoostMode $ program
+    distressSignalCoordinates = last . runProgramInMode 2 $ program
     part2 = show distressSignalCoordinates
 
 solve :: IO (Either String (String, String))
