f6c7420e6d8bd7c0c072cdddd9fe9b35fc64346c
Chris LaRose <cjlarose@gmail.com>
2019-12-17 09:14:51 -0800

    Use Control.Monad.State.gets to get individual record fields

--- src/Advent2019/Intcode/Machine.hs
+++ src/Advent2019/Intcode/Machine.hs
@@ -11,7 +11,7 @@ module Advent2019.Intcode.Machine
   , updateRelativeBase
   ) where
 
-import Control.Monad.State (get, put, modify)
+import Control.Monad.State (gets, put, modify)
 import qualified Data.Map.Strict as Map
 
 import Advent2019.Intcode (IntcodeCompute, instructionPointer, Machine(..), MachineState(..))
@@ -28,7 +28,7 @@ newMachine program input = Machine { instructionPointer = 0
 
 updateMemory :: [(Integer, Integer)] -> IntcodeCompute ()
 updateMemory updates = do
-  currentMemory <- memory <$> get
+  currentMemory <- gets memory
   let newMemory = foldr (uncurry Map.insert) currentMemory updates
   modify $ (\x -> x { memory = newMemory })
 
@@ -36,25 +36,25 @@ writeToAddress :: Integer -> Integer -> IntcodeCompute ()
 writeToAddress addr val = updateMemory [(addr, val)]
 
 valueAtAddress :: Integer -> IntcodeCompute Integer
-valueAtAddress addr = maybe 0 id . Map.lookup addr . memory <$> get
+valueAtAddress addr = maybe 0 id . Map.lookup addr <$> gets memory
 
 readInput :: IntcodeCompute Integer
-readInput = (head . input <$> get) <* (modify $ (\x -> x { input = tail . input $ x }))
+readInput = (head <$> gets input) <* (modify $ (\x -> x { input = tail . input $ x }))
 
 readInstructionPointer :: IntcodeCompute Integer
-readInstructionPointer = instructionPointer <$> get
+readInstructionPointer = gets instructionPointer
 
 updateInstructionPointer :: (Integer -> Integer) -> IntcodeCompute ()
 updateInstructionPointer f = modify $ (\x -> x { instructionPointer = f $ instructionPointer x })
 
 getStatus :: IntcodeCompute MachineState
-getStatus = state <$> get
+getStatus = gets state
 
 setTerminated :: IntcodeCompute ()
 setTerminated =  modify $ (\m -> m { state = Terminated })
 
 getRelativeBase :: IntcodeCompute Integer
-getRelativeBase = relativeBase <$> get
+getRelativeBase = gets relativeBase
 
 updateRelativeBase :: Integer -> IntcodeCompute ()
 updateRelativeBase d = modify $ (\m -> m { relativeBase = relativeBase m + d })
