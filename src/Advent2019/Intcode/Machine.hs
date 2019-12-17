37b8eebfa4096916c42b0da95d96af2372220124
Chris LaRose <cjlarose@gmail.com>
2019-12-17 09:15:37 -0800

    Fix redundant function application

--- src/Advent2019/Intcode/Machine.hs
+++ src/Advent2019/Intcode/Machine.hs
@@ -30,7 +30,7 @@ updateMemory :: [(Integer, Integer)] -> IntcodeCompute ()
 updateMemory updates = do
   currentMemory <- gets memory
   let newMemory = foldr (uncurry Map.insert) currentMemory updates
-  modify $ (\x -> x { memory = newMemory })
+  modify (\x -> x { memory = newMemory })
 
 writeToAddress :: Integer -> Integer -> IntcodeCompute ()
 writeToAddress addr val = updateMemory [(addr, val)]
@@ -39,22 +39,22 @@ valueAtAddress :: Integer -> IntcodeCompute Integer
 valueAtAddress addr = maybe 0 id . Map.lookup addr <$> gets memory
 
 readInput :: IntcodeCompute Integer
-readInput = (head <$> gets input) <* (modify $ (\x -> x { input = tail . input $ x }))
+readInput = (head <$> gets input) <* modify (\x -> x { input = tail . input $ x })
 
 readInstructionPointer :: IntcodeCompute Integer
 readInstructionPointer = gets instructionPointer
 
 updateInstructionPointer :: (Integer -> Integer) -> IntcodeCompute ()
-updateInstructionPointer f = modify $ (\x -> x { instructionPointer = f $ instructionPointer x })
+updateInstructionPointer f = modify (\x -> x { instructionPointer = f $ instructionPointer x })
 
 getStatus :: IntcodeCompute MachineState
 getStatus = gets state
 
 setTerminated :: IntcodeCompute ()
-setTerminated =  modify $ (\m -> m { state = Terminated })
+setTerminated =  modify (\m -> m { state = Terminated })
 
 getRelativeBase :: IntcodeCompute Integer
 getRelativeBase = gets relativeBase
 
 updateRelativeBase :: Integer -> IntcodeCompute ()
-updateRelativeBase d = modify $ (\m -> m { relativeBase = relativeBase m + d })
+updateRelativeBase d = modify (\m -> m { relativeBase = relativeBase m + d })
