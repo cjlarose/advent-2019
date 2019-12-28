43605dfd82c6e90bf531a2ed470649c704e63465
Chris LaRose <cjlarose@gmail.com>
2019-12-28 02:00:06 -0700

    Compute points pulled by beam more efficiently

--- src/Advent2019/Day19.hs
+++ src/Advent2019/Day19.hs
@@ -1,6 +1,7 @@
 module Advent2019.Day19
   ( solve
   , pulledByTractorBeam
+  , nearPoints
   ) where
 
 import Data.Set (Set)
@@ -12,48 +13,57 @@ import Advent2019.Intcode.Parse (program)
 import Advent2019.Intcode.Execute (evalMachine, runMachine)
 import Advent2019.Intcode.Machine (newMachine)
 
-pulledByTractorBeam :: ([TapeSymbol] -> Machine) -> (TapeSymbol, TapeSymbol) -> Bool
+pulledByTractorBeam :: ([TapeSymbol] -> Machine) -> (Int, Int) -> Bool
 pulledByTractorBeam machineFactory (x, y) = (1 ==) . head . snd . evalMachine runMachine $ machine
   where
     machine :: Machine
-    machine = machineFactory [x, y]
+    machine = machineFactory [fromIntegral x, fromIntegral y]
 
 norm :: (Floating a1, Integral a2, Integral a3) => (a2, a3) -> a1
 norm (x0, y0) = sqrt $ fromIntegral x0 ^ 2 + fromIntegral y0 ^ 2
 
-affectedPoints :: ([TapeSymbol] -> Machine) -> [(TapeSymbol, TapeSymbol)]
-affectedPoints machineFactory = search Set.empty $ Set.singleton (0, (0, 0))
+nearPoints :: (Int, Int) -> [(Int, Int)]
+nearPoints (x0, y0) = search Set.empty $ Set.singleton (0, (x0, y0))
   where
-    search visited queue = if pulled
-                           then minCoord : morePoints
-                           else morePoints
+    distance (x1, y1) = sqrt $ (fromIntegral x1 - fromIntegral x0) ^ 2 + (fromIntegral y1 - fromIntegral y0) ^ 2
+    search visited queue = if notYetVisited then minCoord : morePoints else morePoints
       where
         minItem = Set.findMin queue
         (_, minCoord) = minItem
         (x, y) = minCoord
-        pulled = not (minCoord `Set.member` visited) && pulledByTractorBeam machineFactory minCoord
-        neighbors = Set.fromList $ map (\p -> (norm p, p)) [(x + 1, y), (x, y + 1)]
+        notYetVisited = not $ minCoord `Set.member` visited
+        neighbors = Set.fromList $ map (\p -> (distance p, p)) [(x + 1, y), (x, y + 1)]
         newQueue = Set.union neighbors (Set.delete minItem queue)
         morePoints = search (Set.insert minCoord visited) newQueue
 
-santasShip :: ([TapeSymbol] -> Machine) -> (TapeSymbol, TapeSymbol)
-santasShip machineFactory = search (affectedPoints machineFactory) Set.empty
+affectedPoints :: ([TapeSymbol] -> Machine) -> [(Int, Int)]
+affectedPoints machineFactory = (0, 0) : search Set.empty (0, 0)
   where
-    search (p:ps) visited = if bottomRightOfShip
-                            then topLeftOfShip
-                            else search ps $ Set.insert p visited
+    search visited p = (nearestNeighbor : friends) ++ search newVisited nearestNeighbor
       where
-        (x, y) = p
-        seen p0 = Set.member p0 visited
-        bottomRightOfShip = seen (x - 50, y) && seen (x, y - 50)
-        topLeftOfShip = (x - 50, y - 50)
+        nearestNeighbor = head . filter (\n -> not (n `Set.member` visited) && pulledByTractorBeam machineFactory n) . tail . nearPoints $ p
+        (x, y) = nearestNeighbor
+        friends = takeWhile (pulledByTractorBeam machineFactory) [(x1, y) | x1 <- [x + 1..]]
+        newVisited = Set.union visited . Set.fromList $ nearestNeighbor : friends
+
+-- santasShip :: ([TapeSymbol] -> Machine) -> (TapeSymbol, TapeSymbol)
+-- santasShip machineFactory = search (affectedPoints machineFactory) Set.empty
+--   where
+--     search (p:ps) visited = if bottomRightOfShip
+--                             then topLeftOfShip
+--                             else search ps $ Set.insert p visited
+--       where
+--         (x, y) = p
+--         seen p0 = Set.member p0 visited
+--         bottomRightOfShip = seen (x - 50, y) && seen (x, y - 50)
+--         topLeftOfShip = (x - 50, y - 50)
 
 printResults :: [TapeSymbol] -> (String, String)
 printResults program = (part1, part2)
   where
     machineFactory = newMachine program
-    numPoints = length . filter (\(x, y) -> x < 50 && y < 50) . takeWhile (\p -> norm p <= norm (49, 49)) . affectedPoints $ machineFactory
-    part1 = show numPoints
+    closePoints = filter (\(x, y) -> x < 50 && y < 50) . takeWhile (\p -> norm p <= norm (49, 49)) . affectedPoints $ machineFactory
+    part1 = show . length $ closePoints
     -- (x, y) = santasShip machineFactory
     -- part2 = show $ x * 10000 + y
     part2 = "not yet implemented"
