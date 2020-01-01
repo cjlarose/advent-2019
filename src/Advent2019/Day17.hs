f8108c1d1cf94f0b5c134e031028b97a5826b026
Chris LaRose <cjlarose@gmail.com>
2020-01-01 15:00:38 -0800

    Parse vacuum position and direction

--- src/Advent2019/Day17.hs
+++ src/Advent2019/Day17.hs
@@ -2,6 +2,7 @@ module Advent2019.Day17
   ( solve
   ) where
 
+import Data.List (find)
 import qualified Data.Set as Set
 
 import Advent2019.Input (getProblemInputAsByteString, withSuccessfulParse)
@@ -9,12 +10,22 @@ import Advent2019.Intcode (TapeSymbol)
 import Advent2019.Intcode.Parse (program)
 import Advent2019.Intcode.Execute (runMachineWithInput)
 
+data Vacuum = Vacuum { position :: (Int, Int)
+                      , direction :: (Int, Int) } deriving Show
+
 data Scene = Scene { n :: Int
                    , m :: Int
-                   , scaffold :: Set.Set (Int, Int) } deriving Show
+                   , scaffold :: Set.Set (Int, Int)
+                   , vacuum :: Vacuum } deriving Show
 
 parseAsciiOutput :: [TapeSymbol] -> String
 parseAsciiOutput = map (toEnum . fromIntegral)
+
+parseChars :: [String] -> [((Int, Int), Char)]
+parseChars = concat . zipWith parseRow [0..]
+  where
+    parseRow :: Int -> String -> [((Int, Int), Char)]
+    parseRow i = zipWith (\j col -> ((i, j), col)) [0..]
 
 parseScaffold :: [String] -> Set.Set (Int, Int)
 parseScaffold = Set.unions . zipWith parseRow [0..]
@@ -24,13 +35,26 @@ parseScaffold = Set.unions . zipWith parseRow [0..]
                                          then Set.insert (i, j) acc
                                          else acc) Set.empty . zip [0..]
 
+parseVacuum :: [((Int, Int), Char)] -> Vacuum
+parseVacuum xs = Vacuum position direction
+  where
+    isVaccum (_, c) = c == '^' || c == '>' || c == 'v' || c == '<'
+    Just (position, vacChar) = find isVaccum xs
+    direction = case vacChar of
+                  '^' -> (1, 0)
+                  '>' -> (0, 1)
+                  'v' -> (-1, 0)
+                  '<' -> (0, -1)
+
 newScene :: String -> Scene
-newScene ascii = Scene n m scaffold
+newScene ascii = Scene n m scaffold vacuum
   where
     rows = filter (not . null) . lines $ ascii
     n = length rows
     m = length . head $ rows
+    content = parseChars rows
     scaffold = parseScaffold rows
+    vacuum = parseVacuum content
 
 scaffoldIntersections :: Scene -> Set.Set (Int, Int)
 scaffoldIntersections scene = Set.filter isIntersection . scaffold $ scene
